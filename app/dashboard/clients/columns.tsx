d74600909162fd36697d4bb6aab00ca0243487a6
NeilRow56 <neilrowland56@gmail.com>
2024-09-12 15:23:46 +0100

    Client data table amended to give color coding on status

--- app/dashboard/clients/columns.tsx
+++ app/dashboard/clients/columns.tsx
@@ -27,19 +27,9 @@ export type Client = {
     | "charity"
     | "other";
   status: "awaiting_ml_checks" | "active" | "archived";
+  workSuspended: true | false;
 };
 
-function getStatusColor(status: string) {
-  if (status === "FINISHED") {
-    return "bg-green-600";
-  } else if (status === "REFUSED") {
-    return "bg-red-500";
-  } else if (status === "active") {
-    return "bg-yellow-500";
-  } else {
-    return "bg-blue-500";
-  }
-}
 export const columns: ColumnDef<Client>[] = [
   {
     accessorKey: "name",
@@ -75,43 +65,46 @@ export const columns: ColumnDef<Client>[] = [
     accessorKey: "status",
     header: ({ column }) => {
       return (
-        <Button
-          className="px-0"
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Status
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
+        <div className="flex justify-center">
+          <Button
+            className="px-0"
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Status
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        </div>
       );
     },
-    // cell: ({ row }) => {
-    //   const value: string = row.getValue("event_status");
-    //   const color = getStatusColor(value);
-    //   return (
-    //     <div className={cn("rounded-full px-2 text-white", color)}>{value}</div>
-    //   );
-    // },
+    cell: ({ row }) => (
+      <div
+        className={cn(
+          "rounded-lg p-2 text-center capitalize",
+          row.original.status === "active" &&
+            "bg-emerald-400/10 text-emerald-500",
+          row.original.status === "archived" && "bg-red-400/10 text-red-500",
+          row.original.status === "awaiting_ml_checks" &&
+            "bg-amber-400/10 text-amber-500",
+        )}
+      >
+        {row.original.status}
+      </div>
+    ),
   },
 
   {
-    accessorKey: "workSuspended",
-    header: ({ column }) => {
-      return (
-        <Button
-          className="px-0"
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Stop!
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
-  },
-  {
     id: "actions",
-    header: "Actions",
+
+    header: () => (
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Actions
+      </div>
+    ),
     cell: ({ row }) => {
       // IMPORTANT THIS IS THE CLIENT DATA FROM WHICH YOU CAN GET THE ID
       const client = row.original;
@@ -119,9 +112,11 @@ export const columns: ColumnDef<Client>[] = [
       return (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button size="icon" variant="ghost">
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
+            <div className="flex justify-center">
+              <Button size="icon" variant="ghost">
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </div>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
             <DropdownMenuLabel>Actions</DropdownMenuLabel>
