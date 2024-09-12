8786322a42cfc70eaa4e9a0f0d40b8bb98b7ae6c
NeilRow56 <neilrowland56@gmail.com>
2024-09-12 17:03:12 +0100

    ClientIDPage with EmptyState and data set at 0

--- app/dashboard/clients/[clientId]/page.tsx
+++ app/dashboard/clients/[clientId]/page.tsx
@@ -1,5 +1,94 @@
-import React from "react";
+import { notFound, redirect } from "next/navigation";
 
-export default function IndividualClientPage() {
-  return <div>Individual Client Page</div>;
+import db from "@/lib/db";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Book, Edit, PlusCircle } from "lucide-react";
+import { EmptyState } from "@/components/dashboard/EmptyState";
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+async function getClientDetails(clientId: string) {
+  const clientDetails = await db.client.findUnique({
+    where: {
+      id: clientId,
+    },
+  });
+
+  return clientDetails;
+}
+
+export default async function ClientIDPage({
+  params,
+}: {
+  params: { clientId: string };
+}) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user?.id) return redirect("/");
+
+  let data = 0;
+  const clientDetails = await getClientDetails(params.clientId);
+  return (
+    <>
+      <div className="container flex w-full justify-end gap-x-4">
+        <Button asChild variant="secondary">
+          <Link href={"/"}>
+            <Book className="mr-2 size-4" />
+            View What?
+          </Link>
+        </Button>
+        <Button asChild variant="secondary">
+          <Link href={`/dashboard/clients/${params.clientId}/edit`}>
+            <Edit className="mr-2 size-4" />
+            Edit
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link href={`/dashboard/clients/${params.clientId}/createFile`}>
+            <PlusCircle className="mr-2 size-4" />
+            Create File
+          </Link>
+        </Button>
+      </div>
+      <div className="container flex w-full">
+        <div className="my-8">
+          <h2 className="text-3xl font-bold text-primary">
+            {clientDetails?.name}
+          </h2>
+        </div>
+      </div>
+
+      {data === undefined || data === 0 ? (
+        <EmptyState
+          title="You dont have any Files created"
+          description="You currently dont have any files. When created you can see them right here"
+          buttonText="Create File"
+          href={`/dashboard/clients/${params.clientId}/createFile`}
+        />
+      ) : (
+        <div className="max-auto container">
+          <Card>
+            <CardHeader>
+              <CardTitle className="mb-2 font-bold text-primary">
+                Files
+              </CardTitle>
+              <CardDescription>
+                Manage your Files in a simple and intuitive interface
+              </CardDescription>
+            </CardHeader>
+            <CardContent>CLIENT FILES DATA</CardContent>
+          </Card>
+        </div>
+      )}
+    </>
+  );
 }
