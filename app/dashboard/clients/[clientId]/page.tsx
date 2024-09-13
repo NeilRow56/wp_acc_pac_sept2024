ffae010cc8f85f14e1f1ffdefc1e551ec5faa5e2
NeilRow56 <neilrowland56@gmail.com>
2024-09-13 05:15:43 +0100

    Added table to display client files - NONE created at this stage

--- app/dashboard/clients/[clientId]/page.tsx
+++ app/dashboard/clients/[clientId]/page.tsx
@@ -15,6 +15,8 @@ import { Book, Edit, PlusCircle } from "lucide-react";
 import { EmptyState } from "@/components/dashboard/EmptyState";
 
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ClientFilesDataTable } from "@/components/client/ClientFilesDataTable";
+import { columns } from "./columns";
 
 async function getClientDetails(clientId: string) {
   const clientDetails = await db.client.findUnique({
@@ -26,6 +28,35 @@ async function getClientDetails(clientId: string) {
   return clientDetails;
 }
 
+async function getData(userId: string, clientId: string) {
+  const data = await db.yearFile.findMany({
+    where: {
+      userId: userId,
+      clientId: clientId,
+    },
+    select: {
+      period: true,
+      slug: true,
+      createdAt: true,
+      shortDate: true,
+      periodStart: true,
+      periodEnd: true,
+      clientId: true,
+      id: true,
+      Client: {
+        select: {
+          name: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
+}
+
 export default async function ClientIDPage({
   params,
 }: {
@@ -35,11 +66,12 @@ export default async function ClientIDPage({
   const user = await getUser();
   if (!user?.id) return redirect("/");
 
-  let data = 0;
+  // let data = 0;
   const clientDetails = await getClientDetails(params.clientId);
+  const data = await getData(user.id, params.clientId);
   return (
     <>
-      <div className="container flex w-full justify-end gap-x-4">
+      <div className="container flex w-full justify-end gap-x-4 px-4">
         <Button asChild variant="secondary">
           <Link href={"/"}>
             <Book className="mr-2 size-4" />
@@ -59,7 +91,7 @@ export default async function ClientIDPage({
           </Link>
         </Button>
       </div>
-      <div className="container flex w-full">
+      <div className="container mx-auto flex w-full">
         <div className="my-8">
           <h2 className="text-3xl font-bold text-primary">
             {clientDetails?.name}
@@ -67,15 +99,17 @@ export default async function ClientIDPage({
         </div>
       </div>
 
-      {data === undefined || data === 0 ? (
-        <EmptyState
-          title="You dont have any Files created"
-          description="You currently dont have any files. When created you can see them right here"
-          buttonText="Create File"
-          href={`/dashboard/clients/${params.clientId}/createFile`}
-        />
+      {data === undefined || data.length === 0 ? (
+        <div className="mx-auto w-[800px]">
+          <EmptyState
+            title="You dont have any Files created"
+            description="You currently dont have any files. When created you can see them right here"
+            buttonText="Create File"
+            href={`/dashboard/clients/${params.clientId}/createFile`}
+          />
+        </div>
       ) : (
-        <div className="max-auto container">
+        <div className="container mx-auto">
           <Card>
             <CardHeader>
               <CardTitle className="mb-2 font-bold text-primary">
@@ -85,7 +119,9 @@ export default async function ClientIDPage({
                 Manage your Files in a simple and intuitive interface
               </CardDescription>
             </CardHeader>
-            <CardContent>CLIENT FILES DATA</CardContent>
+            <CardContent>
+              <ClientFilesDataTable columns={columns} data={data} />
+            </CardContent>
           </Card>
         </div>
       )}
