859aa1f8ff2e9422ba26e49bfe2d1a498a861e24
NeilRow56 <neilrowland56@gmail.com>
2024-09-21 07:24:01 +0100

    Added initial planning section - tax only. All working ok

---
+++ app/actions/planning.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import db from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import { parseWithZod } from "@conform-to/zod";
+import { CreatePlanningSchema } from "../schemas/workingPapers";
+
+export async function createPlanningAction(prevState: any, formData: FormData) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/api/auth/login");
+  }
+
+  const submission = parseWithZod(formData, {
+    schema: CreatePlanningSchema,
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  const data = await db.planningFile.create({
+    data: {
+      taxOne: submission.value.taxOne,
+      taxOneCompleted: submission.value.taxOneCompleted,
+      taxTwo: submission.value.taxTwo,
+      taxTwoCompleted: submission.value.taxTwoCompleted,
+      taxThree: submission.value.taxThree,
+      taxThreeCompleted: submission.value.taxThreeCompleted,
+      taxFour: submission.value.taxFour,
+      taxFourCompleted: submission.value.taxFourCompleted,
+      taxFive: submission.value.taxFive,
+      taxFiveCompleted: submission.value.taxFiveCompleted,
+      taxSix: submission.value.taxSix,
+      taxSixCompleted: submission.value.taxSixCompleted,
+      taxSeven: submission.value.taxSeven,
+      taxSevenCompleted: submission.value.taxSevenCompleted,
+      yearFileId: formData.get("yearFileId") as string,
+    },
+  });
+
+  return redirect(
+    `/dashboard/clients/${formData.get("clientId")}/${formData.get("yearFileId")}`,
+  );
+}
--- app/dashboard/clients/[clientId]/[yearFileId]/planning/page.tsx
+++ app/dashboard/clients/[clientId]/[yearFileId]/planning/page.tsx
@@ -1,30 +1,305 @@
-import { Button } from "@/components/ui/button";
-import CreateWorkProgrammeForm from "@/components/working_papers/CreateWorkProgrammeForm";
-import { ChevronLeft } from "lucide-react";
-import Link from "next/link";
-import React from "react";
+"use client";
 
-export default function PlanningSummaryPage({
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import React, { useState } from "react";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+
+import { SubmitButton } from "@/components/shared/SubmitButon";
+
+import { useFormState } from "react-dom";
+
+import { Switch } from "@/components/ui/switch";
+import { createPlanningAction } from "@/app/actions/planning";
+import { CreatePlanningSchema } from "@/app/schemas/workingPapers";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+
+export default function CreatePlanningPage({
   params,
 }: {
-  params: { yearFileId: string; clientId: string };
+  params: { clientId: string; yearFileId: string };
 }) {
+  const [lastResult, action] = useFormState(createPlanningAction, undefined);
+  const [form, fields] = useForm({
+    lastResult,
+
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: CreatePlanningSchema });
+    },
+
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
+
   return (
     <>
-      <div className="flex items-center gap-4">
-        <Button variant="outline" size="icon" asChild>
+      <div className="mb-12 flex items-center">
+        <Button size="icon" variant="outline" asChild className="mr-12">
           <Link
             href={`/dashboard/clients/${params.clientId}/${params.yearFileId}`}
           >
-            <ChevronLeft className="h-4 w-4" />
+            <ArrowLeft className="size-4" />
           </Link>
         </Button>
-        <h1 className="text-xl font-semibold tracking-tight"></h1>Working papers
-        - index
+        <h1 className="text-2xl font-semibold text-primary">Year File</h1>
       </div>
-
-      <div className="mt-16 flex flex-col">
-        <CreateWorkProgrammeForm />
+      <div className="flex flex-grow flex-col items-center">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle className="space-y-2 text-3xl font-bold text-primary">
+              AUDIT EXEMPTION / ASSURANCE PROGRAMME - PLANNING SUMMARY
+            </CardTitle>
+            <CardDescription>
+              Edit programme information in the form below.
+            </CardDescription>
+          </CardHeader>
+          <Label className="flex w-4/6 p-4 text-lg font-bold">
+            Schedule the accounts preparation work undertaken together with
+            analytical review which enables the accounts to be compiled. Where
+            applicable, schedule the assurance work undertaken which enables an
+            assurance report to be issued.
+          </Label>
+          <form
+            id={form.id}
+            onSubmit={form.onSubmit}
+            action={action}
+            className=""
+          >
+            <CardContent>
+              <input type="hidden" name="clientId" value={params.clientId} />
+              <input
+                type="hidden"
+                name="yearFileId"
+                value={params.yearFileId}
+              />
+              <div className="flex w-full">
+                <div className="flex w-5/6 flex-col gap-6">
+                  <div className="mr-5 flex flex-col gap-3">
+                    <Label className="text-lg text-primary">
+                      Taxation, Payroll & VAT
+                    </Label>
+                    <div className="flex w-5/6 items-center gap-8">
+                      <div className="text-muted-foreground">1.</div>
+                      <Input
+                        type="text"
+                        key={fields.taxOne.key}
+                        name={fields.taxOne.name}
+                        className="w-full"
+                        placeholder=""
+                        defaultValue={
+                          "Prepare tax computations and proof of tax charge."
+                        }
+                      />
+                      <p className="text-red-500">{fields.taxOne.errors}</p>
+                      <div className="w1/6 i flex h-12 flex-col items-center justify-center">
+                        <Switch
+                          key={fields.taxOneCompleted.key}
+                          name={fields.taxOneCompleted.name}
+                          defaultValue={fields.taxOneCompleted.initialValue}
+                        />
+                        <p className="text-red-500">
+                          {fields.taxOneCompleted.errors}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-full">
+                <div className="flex w-5/6 flex-col gap-6">
+                  <div className="mr-5 flex flex-col gap-3">
+                    <div className="flex w-5/6 items-center gap-8">
+                      <div className="text-muted-foreground">2.</div>
+                      <Input
+                        type="text"
+                        className="w-full"
+                        placeholder=""
+                        key={fields.taxTwo.key}
+                        name={fields.taxTwo.name}
+                        defaultValue={
+                          "Ensure any non-allowable expenditure is identified"
+                        }
+                      />
+                      <p className="text-red-500">{fields.taxTwo.errors}</p>
+                      <div className="w1/6 i flex h-12 flex-col items-center justify-center">
+                        <Switch
+                          key={fields.taxTwoCompleted.key}
+                          name={fields.taxTwoCompleted.name}
+                          defaultValue={fields.taxTwoCompleted.initialValue}
+                        />
+                        <p className="text-red-500">
+                          {fields.taxTwoCompleted.errors}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-full">
+                <div className="flex w-5/6 flex-col gap-6">
+                  <div className="mr-5 flex flex-col gap-3">
+                    <div className="flex w-5/6 items-center gap-8">
+                      <div className="text-muted-foreground">3.</div>
+                      <Input
+                        type="text"
+                        key={fields.taxThree.key}
+                        name={fields.taxThree.name}
+                        className="w-full"
+                        placeholder=""
+                        defaultValue={
+                          "Ensure last years CT provision is cleared by payment, or agreed as outstanding"
+                        }
+                      />
+                      <p className="text-red-500">{fields.taxThree.errors}</p>
+                      <div className="w1/6 i flex h-12 flex-col items-center justify-center">
+                        <Switch
+                          key={fields.taxThreeCompleted.key}
+                          name={fields.taxThreeCompleted.name}
+                          defaultValue={fields.taxThreeCompleted.initialValue}
+                        />
+                        <p className="text-red-500">
+                          {fields.taxThreeCompleted.errors}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-full">
+                <div className="flex w-5/6 flex-col gap-6">
+                  <div className="mr-5 flex flex-col gap-3">
+                    <div className="flex w-5/6 items-center gap-8">
+                      <div className="text-muted-foreground"> 4.</div>
+                      <Input
+                        type="text"
+                        key={fields.taxFour.key}
+                        name={fields.taxFour.name}
+                        className="w-full"
+                        placeholder=""
+                        defaultValue={
+                          "Reconcile VAT year end balance and Outputs to sales."
+                        }
+                      />
+                      <p className="text-red-500">{fields.taxFour.errors}</p>
+                      <div className="w1/6 i flex h-12 flex-col items-center justify-center">
+                        <Switch
+                          key={fields.taxFourCompleted.key}
+                          name={fields.taxFourCompleted.name}
+                          defaultValue={fields.taxFourCompleted.initialValue}
+                        />
+                        <p className="text-red-500">
+                          {fields.taxFourCompleted.errors}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-full">
+                <div className="flex w-5/6 flex-col gap-6">
+                  <div className="mr-5 flex flex-col gap-3">
+                    <div className="flex w-5/6 items-center gap-8">
+                      <div className="text-muted-foreground">5.</div>
+                      <Input
+                        type="text"
+                        key={fields.taxFive.key}
+                        name={fields.taxFive.name}
+                        className="w-full"
+                        placeholder=""
+                        defaultValue={
+                          "Reconcile payroll charge in accounts with payroll records"
+                        }
+                      />
+                      <p className="text-red-500">{fields.taxFive.errors}</p>
+                      <div className="w1/6 i flex h-12 flex-col items-center justify-center">
+                        <Switch
+                          key={fields.taxFiveCompleted.key}
+                          name={fields.taxFiveCompleted.name}
+                          defaultValue={fields.taxFiveCompleted.initialValue}
+                        />
+                        <p className="text-red-500">
+                          {fields.taxFiveCompleted.errors}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-full">
+                <div className="flex w-5/6 flex-col gap-6">
+                  <div className="mr-5 flex flex-col gap-3">
+                    <div className="flex w-5/6 items-center gap-8">
+                      <div className="text-muted-foreground">6.</div>
+                      <Input
+                        type="text"
+                        key={fields.taxSix.key}
+                        name={fields.taxSix.name}
+                        className="w-full"
+                        placeholder=""
+                        defaultValue={""}
+                      />
+                      <p className="text-red-500">{fields.taxSix.errors}</p>
+                      <div className="w1/6 i flex h-12 flex-col items-center justify-center">
+                        <Switch
+                          key={fields.taxSixCompleted.key}
+                          name={fields.taxSixCompleted.name}
+                          defaultValue={fields.taxSixCompleted.initialValue}
+                        />
+                        <p className="text-red-500">
+                          {fields.taxSixCompleted.errors}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-full">
+                <div className="flex w-5/6 flex-col gap-6">
+                  <div className="mr-5 flex flex-col gap-3">
+                    <div className="flex w-5/6 items-center gap-8">
+                      <div className="text-muted-foreground">7.</div>
+                      <Input
+                        type="text"
+                        key={fields.taxSeven.key}
+                        name={fields.taxSeven.name}
+                        className="w-full"
+                        placeholder=""
+                        defaultValue={""}
+                      />
+                      <p className="text-red-500">{fields.taxSeven.errors}</p>
+                      <div className="w1/6 i flex h-12 flex-col items-center justify-center">
+                        <Switch
+                          key={fields.taxSevenCompleted.key}
+                          name={fields.taxSevenCompleted.name}
+                          defaultValue={fields.taxSevenCompleted.initialValue}
+                        />
+                        <p className="text-red-500">
+                          {fields.taxSevenCompleted.errors}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <SubmitButton text="Create Planning" />
+            </CardFooter>
+          </form>
+        </Card>
       </div>
     </>
   );
